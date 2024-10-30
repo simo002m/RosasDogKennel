f7ab2c79850c4a35c7c4d4c552d4eae5dd9b3ec6
simo002m <simo002m@gmail.com>
2024-10-30 12:41:15 +0100

    Changed some stuff from "dog" to "owner"

--- src/src/Owner.java
+++ src/src/Owner.java
@@ -1,45 +1,43 @@
+import java.util.Date;
 
-    public class Owner {
-        private int fldOwnerId;       // Identifier for the dog
-        private String fldOwnerName;  // Name of the Dog
-        private String fldDogBreed; // Name of the Dog Breed
+public class Owner {
+        private int fldOwnerId;
+        private String fldOwnerName;
+        private Date fldDateOfBirth;
+        private String fldEmail;
+        private String fldPhoneNumber;
+        private String fldZipCode;
 
-        public int getFldOwnerId() {
-            return fldDogId;
-        }
+    public Owner(String fldOwnerName, Date fldDateOfBirth, String fldEmail, String fldPhoneNumber, String fldZipCode) {
+        this.fldOwnerName = fldOwnerName;
+        this.fldDateOfBirth = fldDateOfBirth;
+        this.fldEmail = fldEmail;
+        this.fldPhoneNumber = fldPhoneNumber;
+        this.fldZipCode = fldZipCode;
+    }
 
-        public void setFldDogId(Integer fldDogId) {
-            this.fldDogId = fldDogId;
-        }
-
-        public String getFldDogName() {
-            return fldDogName;
-        }
-
-        public void setFldDogName(String fldDogName) {
-            this.fldDogName = fldDogName;
-        }
-
-        public String getFldDogBreed() {
-            return fldDogBreed;
-        }
-
-        public void setFldDogBreed(String fldDogBreed) {
-            this.fldDogBreed = fldDogBreed;
-        }
-
-        public Owner(int DogId, String DogName, String fldDogBreed) {
-            this.fldDogId = fldDogId;
-            this.fldDogName = fldDogName;
-            this.fldDogBreed = fldDogBreed;
-
-        }
-
-        public Owner() {
-        }
-
-
+    public Owner() {
 
     }
 
+    public int getFldOwnerId() {
+            return fldOwnerId;
+        }
 
+    public Date getFldDogBreed() {
+        return fldDogBreed;
+    }
+
+    public String getFldDogName() {
+            return fldDogName;
+        }
+
+    public void setFldDogName(String fldDogName) {
+            this.fldDogName = fldDogName;
+        }
+
+    public void setFldDogBreed(String fldDogBreed) {
+            this.fldDogBreed = fldDogBreed;
+        }
+
+}
